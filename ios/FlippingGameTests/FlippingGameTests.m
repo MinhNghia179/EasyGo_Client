db8a904a002b79d5645e1efd6086d4a248521690
Minh Nghia <nguyenminhnghia.thd@gmail.com>
2022-10-13 15:23:36 +0700

    rename project

--- ios/FlippingGameTests/FlippingGameTests.m
+++ ios/FlippingGameTests/FlippingGameTests.m
@@ -7,11 +7,11 @@
 #define TIMEOUT_SECONDS 600
 #define TEXT_TO_LOOK_FOR @"Welcome to React"
 
-@interface FlippingGameTests : XCTestCase
+@interface EasyGoTests : XCTestCase
 
 @end
 
-@implementation FlippingGameTests
+@implementation EasyGoTests
 
 - (BOOL)findSubviewInView:(UIView *)view matching:(BOOL (^)(UIView *view))test
 {
