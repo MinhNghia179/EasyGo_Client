929e8aa8a1fe373f0ca0ea8e7c3a4702e0c2800f
Minh Nghia <nguyenminhnghia.thd@gmail.com>
2023-01-11 09:03:35 +0700

    update

--- app/screen/Booking/steps/SearchingRide.tsx
+++ app/screen/Booking/steps/SearchingRide.tsx
@@ -1,16 +1,48 @@
 import React from 'react';
 import { Image, View } from 'react-native';
-import { ALERT_TYPE, Toast } from 'react-native-alert-notification';
 import SkeletonPlaceholder from 'react-native-skeleton-placeholder';
+import { useSelector } from 'react-redux';
+import socketIO from 'socket.io-client';
 import PrimaryButton from '../../../components/Button/PrimaryButton';
 import { Text } from '../../../components/Text';
+import { IRootState } from '../../../redux/root-store';
 import { wp } from '../../../services/response-screen-service';
 import { Colors } from '../../../styles/colors';
 import styles from '../../../styles/style-sheet';
+import { SOCKET_API_URL } from '../../../variables/app-config';
 
 interface IProps {}
 
 const SearchingRide = (props: IProps) => {
+  const { portalUser } = useSelector((state: IRootState) => ({
+    portalUser: state.authStore.portalUser,
+  }));
+
+  const socket = socketIO(SOCKET_API_URL, {
+    transports: ['websocket'],
+    jsonp: false,
+  });
+
+  const handleConnectSocket = () => {
+    socket.connect();
+    socket.on('connect', function () {
+      console.log('connected to socket server');
+      socket.emit('auth', portalUser.id);
+    });
+    socket.on('disconnect', function () {
+      console.log('The client has disconnected!');
+    });
+    console.log(socket);
+  };
+
+  const handleGetMessage = () => {
+    console.log(socket.on('chatmsg', data => console.log(data)));
+  };
+
+  const handleChatMessage = () => {
+    socket.emit('chatmsg', 'Hello Minh Nghĩa');
+  };
+
   return (
     <>
       <View style={[styles.flex_row, styles.alg_center, styles.jus_between]}>
@@ -42,6 +74,11 @@ const SearchingRide = (props: IProps) => {
           <SkeletonPlaceholder.Item height={10} />
         </SkeletonPlaceholder>
       </View>
+      <PrimaryButton onPress={handleConnectSocket}>
+        Connect Socket
+      </PrimaryButton>
+      <PrimaryButton onPress={handleChatMessage}>Chat Msg</PrimaryButton>
+      <PrimaryButton onPress={handleGetMessage}>Get Msg</PrimaryButton>
     </>
   );
 };
--- app/services/socket-service.ts
+++ app/services/socket-service.ts
@@ -1,17 +1,10 @@
 import { connect } from 'socket.io-client';
 import { SOCKET_API_URL } from '../variables/app-config';
 
-export const Socket = (userId: string) => {
+export const Socket = (data: string) => {
   const socket = connect(SOCKET_API_URL, {
     transports: ['websocket'],
   });
-  console.log('Start connecting...');
-  socket.on('connect', function () {
-    console.log('Client has connected to the server');
-    socket.emit('auth', userId);
-  });
-  socket.on('disconnect', function () {
-    console.log('The client has disconnected!');
-  });
+
   return socket;
 };
